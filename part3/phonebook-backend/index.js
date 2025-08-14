0a20d32559a08bcefa370ba16c503ddc6ff7a7a7
CaptWorld <vardhanlokesh3@gmail.com>
2025-08-14 12:44:31 +0530

    solution to exercise - 3.6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -56,8 +56,22 @@ app.delete('/api/persons/:id', (request, response) => {
 
 const generateId = () => String(Math.trunc(Math.random() * Number.MAX_SAFE_INTEGER))
 
+const validate = (newPerson) => {
+    if (newPerson.name.trim() === '') {
+        return { status: 400, message: 'name is missing' }
+    } else if (newPerson.number.trim() === '') {
+        return { status: 400, message: 'number is missing' }
+    } else if (persons.find(person => person.name === newPerson.name)) {
+        return { status: 409, message: 'name must be unique' }
+    }
+}
+
 app.post('/api/persons', (request, response) => {
     const person = request.body
+    const error = validate(person)
+    if (error) {
+        return response.status(error.status).json({ error: error.message })
+    }
     person.id = generateId()
     persons = persons.concat(person)
     response.send(person)
