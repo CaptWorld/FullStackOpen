3db38b76482bb4e0143e5da0b4e7bfe19ef2b3d7
CaptWorld <vardhanlokesh3@gmail.com>
2025-08-16 00:26:06 +0530

    solution to exercise - 3.10

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -87,6 +87,6 @@ app.post('/api/persons', (request, response) => {
     response.send(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server started at port ${PORT}`)
