ce3bbd1dc796ca1e40214fee91f814c75c9ec812
CaptWorld <vardhanlokesh3@gmail.com>
2025-08-16 16:46:53 +0530

    solution to exercise - 3.13

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -13,35 +15,12 @@ app.use(morgan(formatter))
 
 app.use(express.static('dist'))
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 app.get('/info', (request, respose) => {
     respose.send(`Phonebook has info for ${persons.length} people <br /><br /> ${new Date()}`)
 })
 
 app.get('/api/persons', (request, response) => {
-    response.send(persons)
+    Person.find({}).then(persons => response.send(persons))
 })
 
 app.get('/api/persons/:id', (request, response) => {
---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const MONGO_URI = process.env.MONGO_URI
+
+mongoose.set('strictQuery', false)
+
+mongoose
+    .connect(MONGO_URI)
+    .then(result => {
+        console.log('Successfully connected to MongoDB')
+    })
+    .catch(error => {
+        console.error("Failed to connect to MongoDB", error)
+    })
+
+const personSchema = mongoose.Schema({
+    name: String,
+    number: String
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObj) => {
+        returnedObj.id = returnedObj._id.toString()
+        delete returnedObj._id
+        delete returnedObj.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
