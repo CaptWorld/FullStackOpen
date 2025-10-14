c14a8667a74ab04eb2a25b7032a9b04d95aa02de
CaptWorld <vardhanlokesh3@gmail.com>
2025-10-15 00:37:36 +0530

    solution to exercise - 4.4

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -8,3 +8,38 @@ test('dummy returns one', () => {
     const result = listHelper.dummy(blogs)
     assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+    test('of empty list is zero', () => {
+        assert.strictEqual(listHelper.totalLikes([]), 0)
+    })
+    test('when list has only one blog equals the likes of that', () => {
+        const likes = 99999999
+        const blogs = [
+            {
+                title: "Not My Life",
+                author: "Lokesh",
+                url: "http://www.lokesh.com",
+                likes
+            }
+        ]
+        assert.strictEqual(listHelper.totalLikes(blogs), likes)
+    })
+    test('of a bigger list is calculated right', () => {
+        const blogs = [
+            {
+                title: "My Life",
+                author: "Lokesh",
+                url: "http://www.lokesh.com",
+                likes: 2
+            },
+            {
+                title: "Not My Life",
+                author: "Lokesh",
+                url: "http://www.lokesh.com",
+                likes: 3
+            }
+        ]
+        assert.strictEqual(listHelper.totalLikes(blogs), 5)
+    })
+})
