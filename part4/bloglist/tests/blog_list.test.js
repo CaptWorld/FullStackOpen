05c3ee78ed1647696b364707430f41f4fb10dc56
CaptWorld <vardhanlokesh3@gmail.com>
2025-10-18 16:23:36 +0530

    solution to execise - 4.9

--- part4/bloglist/tests/blog_list.test.js
+++ part4/bloglist/tests/blog_list.test.js
@@ -19,7 +19,17 @@ test('blogs are returned as JSON', async () => {
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
-    assert.strictEqual(response.body.length, testHelper.initialBlogs.length)
+    const blogs = response.body
+    assert.strictEqual(blogs.length, testHelper.initialBlogs.length)
+})
+
+test('blogs have id field but not _id as identifier', async () => {
+    const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    const blogs = response.body
+    assert.strictEqual(blogs.filter(blog => blog.hasOwnProperty('id') && !blog.hasOwnProperty('_id')).length, testHelper.initialBlogs.length)
 })
 
 after(async () => await mongoose.connection.close())
